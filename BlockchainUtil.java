2240de580d3092909cc4a3fc553535494e4e9ca7
jrodrimo <jrodrimo@users.noreply.github.com>
2025-10-20 00:53:32 +0200

    Update BlockchainUtil.java

--- BlockchainUtil.java
+++ BlockchainUtil.java
@@ -15,7 +15,28 @@ public class BlockchainUtil {
             // Iterate through each block in the blockchain
             for (Block block : blockchain.getChain()) {
                 // Write the image name of the block to the file
-                writer.write("Image Name: " + block.getImageName() + "\n");
+                writer.write("Image Name: " + block.getFileName() + "\n");
+                // Write the hash of the block to the file
+                writer.write("Block Hash: " + block.getBlockHash() + "\n");
+                // Write the hash of the previous block to the file
+                writer.write("Previous Hash: " + block.getPreviousHash() + "\n");
+                // Write a separator to visually separate each block in the file
+                writer.write("---------------------------------\n");
+            }
+        } catch (IOException e) {
+            // Print an error message if there is an issue writing to the file
+            System.err.println("Error writing the blockchain to the file: " + e.getMessage());
+        }
+    }
+
+    public static void saveBlockchainToFile64(String s, Blockchain blockchain, String filename) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filename))) {
+            // Iterate through each block in the blockchain
+            for (Block block : blockchain.getChain()) {
+                // Write the image name of the block to the file
+                writer.write("Image Name: " + block.getFileName() + "\n");
+                // Write the image data to the file
+                writer.write("Image Data: " + s + "\n");
                 // Write the hash of the block to the file
                 writer.write("Block Hash: " + block.getBlockHash() + "\n");
                 // Write the hash of the previous block to the file
